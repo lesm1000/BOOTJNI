36c04f6b5ecb3adf7b944863c450f9b58d0d12e1
eesem <eesem@DESKTOP-3RF33S2>
2018-02-17 22:04:30 +0900

    base commit

---
+++ JNITEST/src/main/java/com/jnitest/JnitestApplication.java
@@ -0,0 +1,12 @@
+package com.jnitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JnitestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JnitestApplication.class, args);
+	}
+}
